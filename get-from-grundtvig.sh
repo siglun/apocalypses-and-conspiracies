dc5a409f9bdbaebd144dd4e385025e24dfd284a0
Sigfrid Lundberg <siggelundberg@gmail.com>
2023-05-28 18:18:52 +0200

    added den yderste dag

--- get-from-grundtvig.sh
+++ get-from-grundtvig.sh
@@ -12,7 +12,7 @@
 QUERY1="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Agv%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
 
 
-GET "$QUERY1"
+GET "$QUERY1" > all-gv.xml
 
 # bible_ref_tesim:"Åb"
 # AND
@@ -26,10 +26,17 @@ GET "$QUERY1"
 
 QUERY2="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Agv%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork%0D%0AAND%0D%0Atext_tesim%3Aragnarok%0D%0A&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
 
-# GET "$QUERY2"
+GET "$QUERY2" > ragnarok-gv.xml
 
 QUERY3="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Agv%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork%0D%0AAND%0D%0Atext_tesim%3Adommedag%0D%0A&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
 
-# GET "$QUERY3"
+GET "$QUERY3" > dommedag-gv.xml
+
+
+QUERY4="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Agv%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork%0D%0AAND%0D%0Atext_tesim%3Aden%20yderste%20dag%0D%0A&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
+
+GET "$QUERY4" > den_yderste_dag-gv.xml
+
+
 
 
--- get-from-kierkegaard.sh
+++ get-from-kierkegaard.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 # bible_ref_tesim:"Åb"
 # AND
 # subcollection_ssi:sks
@@ -12,7 +14,7 @@
 QUERY1="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Asks%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
 
 
-GET "$QUERY1"
+GET "$QUERY1" > all-sks.xml
 
 # bible_ref_tesim:"Åb"
 # AND
@@ -22,14 +24,23 @@ GET "$QUERY1"
 # AND
 # type_ssi:work
 # AND
-#text_tesim:ragnarok
+# text_tesim:ragnarok
 
 QUERY2="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Asks%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork%0D%0AAND%0D%0Atext_tesim%3Aragnarok%0D%0A&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
 
-# GET "$QUERY2"
+GET "$QUERY2" > ragnarok-sks.xml
 
 QUERY3="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Asks%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork%0D%0AAND%0D%0Atext_tesim%3Adommedag%0D%0A&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
 
-# GET "$QUERY3"
+GET "$QUERY3" > dommedag-sks.xml
+
+
+
+QUERY4="https://public-index.kb.dk/solr/text-retriever-core/select/?q=bible_ref_tesim%3A%22%C3%85b%22%0D%0AAND%0D%0Asubcollection_ssi%3Asks%0D%0AAND%0D%0Ais_editorial_ssi%3Ano%0D%0AAND%0D%0Atype_ssi%3Awork%0D%0AAND%0D%0Atext_tesim%3Aden%20yderste%20dag%0D%0A&fq=&fl=*&wt=xml&start=0&rows=1000&sort=+year_itsi+asc+&defType=edismax&indent=on"
+
+GET "$QUERY4" > den_yderste_dag-sks.xml
+
+
+
 
 
